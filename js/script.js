9b5c0c55986b10d6c308c8743f1bea5dd6ada6dd
SquallStrifeX <squallstrifex@gmail.com>
2023-12-01 17:58:19 +0100

    end exercise

--- js/script.js
+++ js/script.js
@@ -8,6 +8,8 @@ for (i=1; i<=5; i++){
     Array_rng.push(rng());   
 }
 
+console.log(Array_rng.length)
+
 let row_num = document.getElementById("number");
 
 for (i=0; i<=Array_rng.length; i++){
@@ -19,11 +21,11 @@ for (i=0; i<=Array_rng.length; i++){
 
 let timer_watch = document.getElementById("timer");
 
-let timer_totale = 3
+let timer_totale = 30
 
-let Array_num_pg = [""]
+let Array_num_pg = []
 
-let point = 0 
+let point = 0
 
 let intervallo = setInterval(function(){
     timer_watch.innerHTML = "Secondi Rimasti:  <br>" + (timer_totale);
@@ -31,26 +33,32 @@ let intervallo = setInterval(function(){
     timer_totale--;
     console.log(timer_totale)
 
-    if(timer_totale == 0){
+    if(timer_totale == -2){
        clearInterval(intervallo);
        row_num.innerHTML = " ";
        timer_watch.innerHTML = " ";
     }
 
-    if(timer_totale == 0){
+    if(timer_totale == -2){
         setTimeout(function(){
-        for(i=0; i<2; i++){
+        for(i=0; i<5; i++){
             let num_pg = parseInt(prompt("Inserisci i numeri apparsi nei 30 secondi"));
              Array_num_pg.push(num_pg)
              console.log(Array_num_pg.length)
        }
-            if(Array_num_pg.length == 3){
-              console.log("ciao")
-              for(i=0; i<Array_rng.length; i++){
+            if(Array_num_pg.length == 5){
+              console.log(Array_num_pg)
+              console.log(Array_rng)
+              for(i=0; i<=Array_num_pg.length; i++){
                     if(Array_rng.includes(Array_num_pg[i])){
                         point++
                         console.log(point)
-        }
+                        row_num.innerHTML = "Hai Totalizzato: " + point + " Punti"
+                        row_num.classList.add("num_rng")
+                    }else {
+                        row_num.innerHTML = "Hai Totalizzato: " + point + " Punti"
+                        row_num.classList.add("num_rng")
+                    }
         
 
      }}}
